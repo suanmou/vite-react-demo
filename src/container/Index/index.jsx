7fa38663c7d80588c6f09b81da7b772959507d62
zhangmin <zhangmin@ly-sky.com>
2021-03-16 17:59:26 +0800

    antd引入，实现按需加载组件

---
+++ src/container/Index/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "antd";
+export default () => {
+  return (
+    <div>
+      <Button type="primary">Index</Button>
+    </div>
+  );
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import vitePluginImp from "vite-plugin-imp";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [
+    reactRefresh(),
+    // vitePluginImp({
+    //   libList: [
+    //     {
+    //       libName: "antd",
+    //       style: (name) => `antd/lib/${name}/style/index.less`,
+    //     },
+    //   ],
+    // }),
+    vitePluginImp({
+      libList: [
+        {
+          libName: "antd",
+          style: (name) => `antd/lib/${name}/style/index.less`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
